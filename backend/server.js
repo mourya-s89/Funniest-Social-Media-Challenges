0e342e8ddfb68f42407581810fededc2e65387fc
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-03-19 15:21:23 +0530

    files

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
-const PORT = 3000;
 
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(8080,async() => {
+  try{
+    await mongoose.connect("mongodb+srv://kadalimouryas89:3iZnt0ysjW59inSz@cluster0.tvfok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("server connected sucessfully");
+  }catch (error){
+    console.log(error);
+  }
 });
