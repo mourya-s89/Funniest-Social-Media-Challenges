8ad27c733fadeeb9ea071b1785a5b168a714118b
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-03-29 11:06:28 +0530

    completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
+import AllRoutes from './AllRoutes'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <AllRoutes/>
+    </BrowserRouter>
+
+    </>
+
+  )
+}
+
+export default App
